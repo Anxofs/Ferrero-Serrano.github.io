8eb8e6250a894a365caffd04c9bc08157c7a054a
Ángel Ferrero-Serrano <30509211+Anxofs@users.noreply.github.com>
2019-08-22 20:56:20 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,7 @@ set -e\
 \
 printf "\\033[0;32mDeploying updates to GitHub...\\033[0m\\n"\
 \
-# Build the project.\
-hugo # if using a theme, replace with `hugo -t <AllinOne>`\
-\
+
 # Go To Public folder\
 cd public\
 \
